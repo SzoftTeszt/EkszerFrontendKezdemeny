f4d385b233683a61a7c2be01b41ad74793f31f72
Erős István <eros@zold.lan>
2025-04-07 15:52:29 +0200

    vege

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  ekszerek:any
+  constructor(private base:BaseService){
+    this.base.getEkszerek().subscribe(
+      (e)=>this.ekszerek=e
+    )
+  }
+
 
 }
